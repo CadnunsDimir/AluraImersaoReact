f224a2c50718841939643b6af7b459887f414b2a
CadnunsDimir <cadnuns.dimir@gmail.com>
2020-07-27 17:20:12 -0300

    aula dia 1: criando menu

---
+++ aluraflix/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './components/menu';
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      teste legal
+    </div>
+  );
+}
+
+export default App;
